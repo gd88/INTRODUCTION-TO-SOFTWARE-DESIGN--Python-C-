d0880d90e3f5eccee4518be263cd36cb77b2d299
wongyo Kim <dnjsry8377@naver.com>
2022-01-21 21:32:37 +0900

    fi

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[10]="";
+    int count=0;
+    while(count<10)
+    {count=count+1;
+    scanf("%s", name);
+    printf("%s",name);
+    }
+    return 0;
+
+}
